9332aa3bf472448b8d87b20c66a6fd98b34315c6
Andres Mena <ballerlikemike23@gmail.com>
2019-05-29 23:43:12 -0400

    Styles for Pokecards and Overall Web-App

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 
 class Pokedex extends Component {
   static defaultProps = {
     pokemon: [
-      {id: 4, name: 'Charmander', type: 'fire', exp: 62},
-      {id: 7, name: 'Squirtle', type: 'water', exp: 63},
-      {id: 11, name: 'Metapod', type: 'bug', exp: 72},
-      {id: 12, name: 'Butterfree', type: 'flying', exp: 178},
-      {id: 25, name: 'Pikachu', type: 'electric', exp: 112},
-      {id: 39, name: 'Jigglypuff', type: 'normal', exp: 95},
-      {id: 94, name: 'Gengar', type: 'poison', exp: 225},
-      {id: 133, name: 'Eevee', type: 'normal', exp: 65}
+      { id: 4, name: 'Charmander', type: 'fire', exp: 62 },
+      { id: 7, name: 'Squirtle', type: 'water', exp: 63 },
+      { id: 11, name: 'Metapod', type: 'bug', exp: 72 },
+      { id: 12, name: 'Butterfree', type: 'flying', exp: 178 },
+      { id: 25, name: 'Pikachu', type: 'electric', exp: 112 },
+      { id: 39, name: 'Jigglypuff', type: 'normal', exp: 95 },
+      { id: 94, name: 'Gengar', type: 'poison', exp: 225 },
+      { id: 133, name: 'Eevee', type: 'normal', exp: 65 }
     ]
   }
 
@@ -19,9 +20,11 @@ class Pokedex extends Component {
     return (
       <div className="Pokedex">
         <h1>Pokedex!</h1>
-        {this.props.pokemon.map((p) => {
-          return <Pokecard id={p.id} name={p.name} type={p.exp} />
-        })}
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => {
+            return <Pokecard id={p.id} name={p.name} type={p.exp} />
+          })}
+        </div>
       </div>
     )
   }
